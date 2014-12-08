b07009f0c59b5c22b93dba218acdd520e70f3049
s413574930 <sxin0413@163.com>
2014-12-08 17:24:29 +0800

    init

---
+++ Chat/src/com/ivan/chat/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.ivan.chat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+	private EditText txtLoginUserName;
+	private EditText txtLoginUserPwd;
+	
+	private Button btnLogin;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		initUI();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+	
+	protected void initUI(){
+		txtLoginUserName = (EditText) findViewById(R.id.txtLoginUserName);
+		txtLoginUserPwd = (EditText) findViewById(R.id.txtLoginUserPwd);
+		btnLogin = (Button) findViewById(R.id.btnLogin);
+	}
+	
+	protected void initListener(){
+		
+	}
+
+}
